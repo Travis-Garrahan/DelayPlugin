be5edc7e86d14d49273c8253dcfd51944bd4f22d
Russell Brown <russellbrown798@gmail.com>
2025-09-30 21:06:21 -0400

    Added basic GUI sliders

--- plugin/include/DelayPlugin/PluginEditor.h
+++ plugin/include/DelayPlugin/PluginEditor.h
@@ -17,6 +17,13 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
+    
+    juce::Slider delayTimeSlider;
+    juce::Label delayTimeLabel;
+    juce::Slider feedbackSlider;
+    juce::Label feedbackLabel;
+    juce::Slider mixSlider;
+    juce::Label mixLabel;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
--- plugin/src/PluginEditor.cpp
+++ plugin/src/PluginEditor.cpp
@@ -8,7 +8,41 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     juce::ignoreUnused (processorRef);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+    
+    // int textBoxWidth = 50;
+
+    // Delay Time
+    addAndMakeVisible(delayTimeSlider);
+    delayTimeSlider.setRange(1.0, 1000.0);
+    delayTimeSlider.setValue(500.0);
+    delayTimeSlider.setNumDecimalPlacesToDisplay(0);
+    // delayTimeSlider.setTextBoxStyle(juce::Slider::TextBoxLeft, false, textBoxWidth, delayTimeSlider.getTextBoxHeight());
+    addAndMakeVisible(delayTimeLabel);
+    delayTimeLabel.setText("Time", juce::dontSendNotification);
+    delayTimeLabel.attachToComponent(&delayTimeSlider, true);
+
+    // Feedback
+    addAndMakeVisible(feedbackSlider);
+    feedbackSlider.setRange(0.0, 0.99);
+    feedbackSlider.setValue(0.5);
+    feedbackSlider.setNumDecimalPlacesToDisplay(3);
+    // feedbackSlider.setTextBoxStyle(juce::Slider::TextBoxLeft, false, textBoxWidth, feedbackSlider.getTextBoxHeight());
+    addAndMakeVisible(feedbackLabel);
+    feedbackLabel.setText("Feedback", juce::dontSendNotification);
+    feedbackLabel.attachToComponent(&feedbackSlider, true);
+
+    // Mix
+    addAndMakeVisible(mixSlider);
+    mixSlider.setRange(0.0, 1.0);
+    mixSlider.setValue(0.5);
+    mixSlider.setNumDecimalPlacesToDisplay(3);
+    // mixSlider.setTextBoxStyle(juce::Slider::TextBoxLeft, false, textBoxWidth, mixSlider.getTextBoxHeight());
+    addAndMakeVisible(mixLabel);
+    mixLabel.setText("Mix", juce::dontSendNotification);
+    mixLabel.attachToComponent(&mixSlider, true);
+
     setSize (400, 300);
+
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -21,13 +55,23 @@ void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    // g.setColour (juce::Colours::white);
+    // g.setFont (15.0f);
+    // g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    int leftMargin = getWidth() / 2 - 100;
+    int sliderWidth = 200;
+    int sliderHeight = 25;
+    int pad = 10;
+
+    // setBound(int x, int y, int width, int height)
+    delayTimeSlider.setBounds(leftMargin, (getHeight() - sliderHeight) / 2 - pad - sliderHeight, sliderWidth, sliderHeight);
+    feedbackSlider.setBounds(leftMargin, (getHeight() - sliderHeight) / 2, sliderWidth, sliderHeight);
+    mixSlider.setBounds(leftMargin, (getHeight() - sliderHeight) / 2 + pad + sliderHeight, sliderWidth, sliderHeight);
+
 }
