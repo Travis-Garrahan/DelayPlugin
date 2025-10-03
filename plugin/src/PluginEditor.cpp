3605009b1beaed33a065b36f7273c5717b8fa70e
Russell Brown <russellbrown798@gmail.com>
2025-10-03 15:36:51 -0400

    improved GUI layout

--- plugin/src/PluginEditor.cpp
+++ plugin/src/PluginEditor.cpp
@@ -9,13 +9,14 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     
-    // int textBoxWidth = 50;
+    //int textBoxWidth = 50;
 
     setSize(400, 300);
 
     // Delay Time
     delayTimeSlider.setNumDecimalPlacesToDisplay(0);
-    // delayTimeSlider.setTextBoxStyle(juce::Slider::TextBoxLeft, false, textBoxWidth, delayTimeSlider.getTextBoxHeight());
+    //delayTimeSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, textBoxWidth, delayTimeSlider.getTextBoxHeight());
+    delayTimeSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
     delayTimeSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
     addAndMakeVisible(delayTimeSlider);
 
@@ -23,37 +24,42 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     delayTimeSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(processorRef.apvts, 
         "DELAY_TIME", delayTimeSlider);
 
-    addAndMakeVisible(delayTimeLabel);
     delayTimeLabel.setText("Time", juce::dontSendNotification);
-    delayTimeLabel.attachToComponent(&delayTimeSlider, true);
+    delayTimeLabel.setJustificationType(juce::Justification::centred);
+    addAndMakeVisible(delayTimeLabel);
+    //delayTimeLabel.attachToComponent(&delayTimeSlider, true);
 
     // Feedback
     feedbackSlider.setNumDecimalPlacesToDisplay(3);
-    // feedbackSlider.setTextBoxStyle(juce::Slider::TextBoxLeft, false, textBoxWidth, feedbackSlider.getTextBoxHeight());
+    //feedbackSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, textBoxWidth, feedbackSlider.getTextBoxHeight());
+    feedbackSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
     feedbackSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
     addAndMakeVisible(feedbackSlider);
 
     feedbackSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(processorRef.apvts, 
         "FEEDBACK", feedbackSlider);
 
-    addAndMakeVisible(feedbackLabel);
     feedbackLabel.setText("Feedback", juce::dontSendNotification);
-    feedbackLabel.attachToComponent(&feedbackSlider, true);
+    feedbackLabel.setJustificationType(juce::Justification::centred);
+    addAndMakeVisible(feedbackLabel);
+    //feedbackLabel.attachToComponent(&feedbackSlider, true);
 
     // Mix
     // mixSlider.setRange(0.0, 1.0);
     // mixSlider.setValue(0.5);
     mixSlider.setNumDecimalPlacesToDisplay(3);
-    // mixSlider.setTextBoxStyle(juce::Slider::TextBoxLeft, false, textBoxWidth, mixSlider.getTextBoxHeight());
+    //mixSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, textBoxWidth, mixSlider.getTextBoxHeight());
+    mixSlider.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
     mixSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
     addAndMakeVisible(mixSlider);
 
     mixSliderAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(processorRef.apvts, 
         "MIX", mixSlider);
 
-    addAndMakeVisible(mixLabel);
     mixLabel.setText("Mix", juce::dontSendNotification);
-    mixLabel.attachToComponent(&mixSlider, true);
+    mixLabel.setJustificationType(juce::Justification::centred);
+    addAndMakeVisible(mixLabel);
+    //mixLabel.attachToComponent(&mixSlider, true);
 
 }
 
@@ -74,17 +80,19 @@ void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 
 void AudioPluginAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
-    int leftMargin = getWidth() / 2 - 100;
-    int sliderWidth = 200;
-    // int sliderHeight = 25;
-    int sliderHeight = 50;
-    int pad = 10;
+    // Lay out the GUI components
+    int sliderWidth = 80;
+    int sliderHeight = 80;
+    int labelWidth = 80;
+    int labelHeight = 15;
 
     // setBound(int x, int y, int width, int height)
-    delayTimeSlider.setBounds(leftMargin, (getHeight() - sliderHeight) / 2 - pad - sliderHeight, sliderWidth, sliderHeight);
-    feedbackSlider.setBounds(leftMargin, (getHeight() - sliderHeight) / 2, sliderWidth, sliderHeight);
-    mixSlider.setBounds(leftMargin, (getHeight() - sliderHeight) / 2 + pad + sliderHeight, sliderWidth, sliderHeight);
+    mixSlider.setBounds( (int)(getWidth() * 0.15), getHeight() / 2 - sliderHeight, sliderWidth, sliderHeight);
+    mixLabel.setBounds( (int)(getWidth() * 0.15), getHeight() / 2, labelWidth, labelHeight);
+    
+    delayTimeSlider.setBounds((int)(getWidth() * 0.85) - sliderWidth, getHeight() / 2 - sliderHeight, sliderWidth, sliderHeight);
+    delayTimeLabel.setBounds((int)(getWidth() * 0.85) - sliderWidth, getHeight() / 2, labelWidth, labelHeight);
 
+    feedbackSlider.setBounds(getWidth() / 2 - sliderWidth / 2, getHeight() / 2, sliderWidth, sliderHeight);
+    feedbackLabel.setBounds(getWidth() / 2 - sliderWidth / 2, getHeight() / 2 + sliderHeight, labelWidth, labelHeight);
 }
